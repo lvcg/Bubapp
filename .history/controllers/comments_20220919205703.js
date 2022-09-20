894f80a70c14829d7a8f68f6a603ec673267cb2a
lvcg <flowfxdesignsonline@gmail.com>
2022-09-20 00:40:21 -0500

    added comments and username

---
+++ .history/controllers/comments_20220919205703.js
@@ -0,0 +1,20 @@
+const Comment = require("../models/Comment");
+
+module.exports = {
+  createComment: async (req, res) => {
+    try {
+      //TODO: ADD NEW PROPERTIES TO EACH DOCUMENT
+            await Comment.create({
+        comment: req.body.comment,
+        likes: 0,
+        post: req.params.id,
+        createdById: req.user.id,
+        created
+      });
+      console.log("Comment has been added!");
+      res.redirect("/post/"+req.params.id);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
---
+++ .history/controllers/comments_20220919210249.js
@@ -0,0 +1,21 @@
+const Comment = require("../models/Comment");
+
+module.exports = {
+  createComment: async (req, res) => {
+    try {
+      //TODO: ADD NEW PROPERTIES TO EACH DOCUMENT
+        const post 
+        await Comment.create({
+        comment: req.body.comment,
+        likes: 0,
+        post: req.params.id,
+        createdById: req.user.id,
+        createdByName: req.user.username,
+      });
+      console.log("Comment has been added!");
+      res.redirect("/post/"+req.params.id);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+};
